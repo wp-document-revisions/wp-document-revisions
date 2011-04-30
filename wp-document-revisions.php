bf9bd5877cf52203e79c8116c359160340c7c9c6
Benjamin J. Balter <ben@balter.com>
2011-04-30 22:35:22 +0000

    Initial commit with plugin file and header.

---
+++ wp-document-revisions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Document Revisions
+Plugin URI: http://
+Description: Document Revisioning and Version Control for WordPress; GSoC 2011.
+Version: 0.0.1
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com
+License: GPL2
+*/
+?>
