ffb6ae7f92b247dc59e036fcb01206014c317043
Benjamin J. Balter <ben@balter.com>
2011-07-25 18:48:01 +0000

    version fix

--- wp-document-revisions.php
+++ wp-document-revisions.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Document Revisions
 Plugin URI: http://
 Description: Document Revisioning and Version Control for WordPress; GSoC 2011.
-Version: 1.0
+Version: 0.5.1
 Author: Benjamin J. Balter
 Author URI: http://ben.balter.com
 License: GPL2
