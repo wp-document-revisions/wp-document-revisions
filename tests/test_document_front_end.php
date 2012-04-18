dc73a39b801d1b6ea6f6b16449212f1825d2e868
Benjamin J. Balter <ben@balter.com>
2012-04-18 18:42:36 -0400

    comments and code cleanup

--- tests/test_document_front_end.php
+++ tests/test_document_front_end.php
@@ -1,33 +1,55 @@
 <?php
+/**
+ * Tests front-end functionality
+ *
+ * @author Benjamin J. Balter <ben@balter.com>
+ * @package WP_Document_Revisions
+ */
 
 class WP_Test_Document_Front_End extends WPTestCase {
 
+	/**
+	 *
+	 */
 	function test_revisions_shortcode() {
-	
-   		$tdr = new WP_Test_Document_Revisions();
-   		$this->markTestIncomplete( 'Not yet implemented' );
-   		
+
+		$tdr = new WP_Test_Document_Revisions();
+		$this->markTestIncomplete( 'Not yet implemented' );
+
 		//$docID = $tdr->test_add_document();
 		//$output = do_shortcode( '[document_revisions number="2" id="' . $docID . '"' );
-	
+
 	}
-	
+
+
+	/**
+	 *
+	 */
 	function test_document_shortcode() {
-   		
-   		$tdr = new WP_Test_Document_Revisions();
-   		$this->markTestIncomplete( 'Not yet implemented' );
-   		
+
+		$tdr = new WP_Test_Document_Revisions();
+		$this->markTestIncomplete( 'Not yet implemented' );
+
 		//$docID = $tdr->test_add_document();
 		//$output = do_shortcode( '[documents]' );
 
 	}
-	
+
+
+	/**
+	 *
+	 */
 	function test_get_documents() {
-   		$this->markTestIncomplete( 'Not yet implemented' );	
+		$this->markTestIncomplete( 'Not yet implemented' );
 	}
-	
+
+
+	/**
+	 *
+	 */
 	function test_get_document_revisions() {
-   		$this->markTestIncomplete( 'Not yet implemented' );
+		$this->markTestIncomplete( 'Not yet implemented' );
 	}
+
 
 }
