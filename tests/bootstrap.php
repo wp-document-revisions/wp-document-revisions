504aca484386a457881671f2064a64a1af269e1d
Ben Balter <ben.balter@github.com>
2014-07-31 18:18:43 -0400

    spike out wp-cli backed tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,14 @@
 <?php
-/**
- * Bootstrap the testing environment
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
- * @package wordpress-plugin-tests
- *
- * Usage: change the below array to any plugin(s) you want activated during the tests
- *        value should be the path to the plugin relative to /wp-content/
- *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
- *
- */
 
-require dirname( __FILE__ ) . '/functions.php';
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'wp-document-revisions/wp-document-revisions.php' ),
-);
+require_once $_tests_dir . '/includes/functions.php';
 
-require dirname( __FILE__ ) . '/lib/bootstrap.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../wp-document-revisions.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
+
