5d80400c3b4e11d4f7a78022658fc384ff3f2ef7
Jeremy Felt <jeremy.felt@gmail.com>
2017-11-07 16:19:55 -0800

    Don't verify posts that don't exist
    If `0` is passed to `verify_post_type()` and a global post object
    is not available, then immediately return `false`.

--- includes/class-wp-document-revisions.php
+++ includes/class-wp-document-revisions.php
@@ -1169,6 +1169,11 @@ class WP_Document_Revisions {
 		}
 
 		$post = get_post( $documentish );
+
+		if ( ! $post ) {
+			return false;
+		}
+
 		$post_type = $post->post_type;
 
 		// if post is really an attachment or revision, look to the post's parent
