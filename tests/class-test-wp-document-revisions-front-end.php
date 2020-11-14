eb17357671853b05bb6a54b4f9744475bb7cc406
NeilWJames <neil.james@lionsheraldic78.org>
2020-11-14 18:55:23 +0100

    Document test script

--- tests/class-test-wp-document-revisions-front-end.php
+++ tests/class-test-wp-document-revisions-front-end.php
@@ -49,7 +49,7 @@ class Test_WP_Document_Revisions_Front_End extends WP_UnitTestCase {
 	 */
 	public function consoleLog( $text ) {
 			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_read_fwrite
-			fwrite( STDERR, "\n" . $text . ' : ');
+			fwrite( STDERR, "\n" . $text . ' : ' );
 	}
 
 	/**
