4b5e282ccf9b058f65aa463009e55879cf1cfe94
NeilWJames <neil.james@lionsheraldic78.org>
2020-11-24 22:35:42 +0100

    Test admin

--- tests/class-test-wp-document-revisions-admin.php
+++ tests/class-test-wp-document-revisions-admin.php
@@ -125,7 +125,8 @@ class Test_WP_Document_Revisions_Admin extends WP_UnitTestCase {
 		$output = ob_get_contents();
 		ob_end_clean();
 
-		$this->assertEquals( 2, (int) substr_count( $output, '<a href' ), 'revision count' );
+		// There will be 2 links to documents plus 1 for RSS feed.
+		$this->assertEquals( 3, (int) substr_count( $output, '<a href' ), 'revision count' );
 		$this->assertEquals( 1, (int) substr_count( $output, '-revision-1.' ), 'revision count revision 1' );
 		$this->assertEquals( 2, (int) substr_count( $output, '-revision-21.' ), 'revision count revision 2' );
 		_destroy_user( $user_id );
