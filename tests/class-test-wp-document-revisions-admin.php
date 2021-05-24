d1523cf4d55b5690cf545d4754cb476719610f72
NeilWJames <neil.james@lionsheraldic78.org>
2021-05-24 21:08:19 +0200

    Align to wp_die_fix for Multisite test

--- tests/class-test-wp-document-revisions-admin.php
+++ tests/class-test-wp-document-revisions-admin.php
@@ -248,13 +248,17 @@ class Test_WP_Document_Revisions_Admin extends Test_Common_WPDR {
 		$wpdr->admin->revision_metabox( $post_obj );
 		$output = ob_get_contents();
 		ob_end_clean();
-		console_log( 'Auth:' . $output );
-		console_log( get_blog_permalink( $post_obj->blog_id, $post_obj->ID ) );
 
 		// There will be 1 for RSS feed.
 		self::assertEquals( 3, (int) substr_count( $output, '<a href' ), 'revision count' );
-		self::assertEquals( 1, (int) substr_count( $output, '-revision-1.' ), 'revision count revision 1' );
-		self::assertEquals( 0, (int) substr_count( $output, '-revision-2.' ), 'revision count revision 2' );
+		// Multisite does not have a pretty permalink.
+		if ( is_multisite() ) {
+			self::assertEquals( 1, (int) substr_count( $output, 'revision=1.txt' ), 'revision count revision 1' );
+			self::assertEquals( 0, (int) substr_count( $output, 'revision=2.txt' ), 'revision count revision 2' );		
+		} else {
+			self::assertEquals( 1, (int) substr_count( $output, '-revision-1.' ), 'revision count revision 1' );
+			self::assertEquals( 0, (int) substr_count( $output, '-revision-2.' ), 'revision count revision 2' );
+		}
 	}
 
 	/**
@@ -274,12 +278,14 @@ class Test_WP_Document_Revisions_Admin extends Test_Common_WPDR {
 		$wpdr->admin->document_metabox( $post_obj );
 		$output = ob_get_contents();
 		ob_end_clean();
-		console_log( 'Metabox:' . $output );
-		console_log( get_permalink( $post_obj->ID ) );
-		console_log( get_blog_permalink( $post_obj->blog_id, $post_obj->ID ) );
 
 		self::assertEquals( 1, (int) substr_count( $output, 'post_id=' . $post_obj->ID . '&' ), 'document metabox post_id' );
-		self::assertEquals( 1, (int) substr_count( $output, get_permalink( $post_obj->ID ) ), 'document metabox permalink' );
+		// Multisite does not have a pretty permalink.
+		if ( is_multisite() ) {
+			self::assertEquals( 1, (int) substr_count( $output, 'post_type=document&#038;p=' . $post_obj->ID ), 'document metabox permalink_ms' );
+		} else {
+			self::assertEquals( 1, (int) substr_count( $output, get_permalink( $post_obj->ID ) ), 'document metabox permalink' );
+		}
 		self::assertEquals( 1, (int) substr_count( $output, get_the_author_meta( 'display_name', self::$editor_user_id ) ), 'document metabox author' );
 	}
 
