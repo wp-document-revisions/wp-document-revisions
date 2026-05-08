baf105591c0ed4bd8b4979fbc3d7a91ebb86693c
NeilWJames <neil.james@lionsheraldic78.org>
2026-05-09 00:15:30 +0200

    Address #496

--- includes/class-wp-document-revisions-validate-structure.php
+++ includes/class-wp-document-revisions-validate-structure.php
@@ -877,13 +877,13 @@ class WP_Document_Revisions_Validate_Structure {
 	 *
 	 * @since 3.4.0
 	 *
-	 * @param int    $attach_id id of an attachment post object.
-	 * @param string $doc_id    id of the document post object.
+	 * @param int $attach_id id of an attachment post object.
+	 * @param int $doc_id    id of the document post object.
 	 * @return int|false
 	 */
-	private static function check_attachment( $attach_id, string $doc_id ) {
+	private static function check_attachment( $attach_id, $doc_id ) {
 		$attach = get_post( $attach_id );
-		if ( ( ! is_object( $attach ) ) || 'attachment' !== $attach->post_type || (int) $doc_id !== $attach->post_parent ) {
+		if ( ( ! is_object( $attach ) ) || 'attachment' !== $attach->post_type || $doc_id !== $attach->post_parent ) {
 			// post_content points to an invalid attachment.
 			return array(
 				'code'  => 2,
